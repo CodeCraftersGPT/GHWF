6a74294510f6212daecbfac56deb89e67299d66d
Code Crafter <kumar@jatanix.com>
2024-08-18 20:42:57 +0530

    label change

--- simple-react-login/src/App.js
+++ simple-react-login/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Login-CC</h2>
+        <h2>Login-CC1</h2>
         <form>
           <label>
             Username:
